20479ef5a97a8cd76b77a356b8d05624ec8de0c0
Fernando Castillo <desarrollo@freengers.com>
2021-08-08 12:48:46 -0500

    TDD: invalid reply comment

--- tests/Feature/ApiPostCommentTest.php
+++ tests/Feature/ApiPostCommentTest.php
@@ -6,10 +6,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Traits\CommentTestable;
 
 class ApiPostCommentTest extends TestCase
 {
     use RefreshDatabase;
+
+    private function get_clean_payload(){
+        return [
+            'name' =>  'Hi my name is Fernando',
+            'comment' => 'And this is a great c0mm3nt'
+        ];
+    }
+
+    private function get_dirty_payload(){
+        return [
+            'name' =>  'Hi I am a dirty  <|`\' string',
+            'comment' => '9393838'
+        ];
+    }
 
     public function test_trying_to_post_an_empty_comment()
     {
@@ -21,26 +36,20 @@ class ApiPostCommentTest extends TestCase
 
     public function test_trying_to_post_dirty_payload(){
 
-        $payload = [
-            'name' =>  'Hi I am a dirty  <|`\' string',
-            'comment' => '9393838'
-        ];
+        $dirtyPayload = $this->get_dirty_payload();
 
         $this
-            ->postJson('/api/comment', $payload)
+            ->postJson('/api/comment', $dirtyPayload)
             ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
         ;
     }
 
     public function test_trying_to_post_clean_payload(){
 
-        $payload = [
-            'name' =>  'Hi my name is Fernando',
-            'comment' => 'And this is a great c0mm3nt'
-        ];
+        $cleanPayload = $this->get_clean_payload();
 
         $this
-            ->postJson('/api/comment', $payload)
+            ->postJson('/api/comment', $cleanPayload)
             ->assertStatus(Response::HTTP_OK)
             ->assertJsonStructure([
                 'message',
@@ -51,6 +60,18 @@ class ApiPostCommentTest extends TestCase
                 ]
             ])
         ;
+    }
+
+    public function test_trying_to_post_a_invalid_reply_comment(){
+
+        $cleanPayload = $this->get_clean_payload();
+        $cleanPayload['parent_id'] = 'INVALID_ID';
+
+        $this
+            ->postJson('/api/comment', $cleanPayload)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ;
+
     }
 
 
